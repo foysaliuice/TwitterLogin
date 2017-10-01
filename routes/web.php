6dd624636f1b9f4c38a96ae441b2df630c9aa0f4
foysaliuice <foysal.iuice@gmail.com>
2017-10-01 21:00:20 +0600

    Add redirect and callback route

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/redirect','SocialAuthTwitterController@redirect')->name('redirect');
+Route::get('/callback','SocialAuthTwitterController@callback')->name('callback');
